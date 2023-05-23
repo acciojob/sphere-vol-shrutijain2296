964b2f8616f993eea29f4ac253b91807e121e91f
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-05-23 16:21:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function volume_sphere() {
+let radius = document.getElementsById("radius");
+	let volume = document.getElementsById("volume");
+	let button = document.getElementsById("submit");
+
+	function volume_sphere() {
     //Write your code here
+	let ans = 4/3 * 3.14 * (radius * radius * radius);
+	volume.append(ans);
   
 } 
 
