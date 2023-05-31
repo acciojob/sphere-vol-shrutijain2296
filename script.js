38d4b03c387fe8db2af411120e217b3466e02bfb
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-05-31 22:01:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 	function volume_sphere() {
     //Write your code here
 	let radius = document.getElementsById("radius");
-	let ans = (4/3) * (Math.PI) * Math.pow(radius, 3);
+	let ans = (4/3) * (22/7) * Math.pow(radius, 3);
 	let volume = document.getElementById("volume");
 	volume.value = ans;
   
